06cb076a559b079331fdf6c9c3fd0ce810f0bbfa
ferranfite <205749644+ferranfite@users.noreply.github.com>
2025-04-09 19:07:23 +0200

    labels vel. i voltes
    s'han afegit els labels vel. i voltes al circuit amb les seves variables corresponents aplicades.

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,6 +3,9 @@ var velocitat :float= 100
 var direccio : Vector2 = Vector2.LEFT
 var gas := 150
 var fre = 300
+var voltes = 0
+
+
 	
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("dreta"):
@@ -22,6 +25,14 @@ func _process(delta: float) -> void:
 	velocity = direccio * velocitat
 	move_and_slide()
 	rotation = direccio.angle()
+	print(velocitat)
+	get_node("/root/Node2D/StaticBody2D/velocitat").text = "Velocitat: " + str(velocitat)
+	
+
+
+
+
+
 	
 			
 			
@@ -37,3 +48,9 @@ func _process(delta: float) -> void:
 	#var velocitat_angular := 0.05
 	
 	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == self:
+		voltes += 1
+		get_node("/root/Node2D/StaticBody2D/VoltesLabel").text = "Voltes: " + str(voltes)
